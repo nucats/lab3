d81ad2465207adc8e31b314c98bf5bd2e9335ecf
nucats <nishabhuva2018@u.northwestern.edu>
2016-01-20 14:42:40 -0600

    working feet to centimeters

--- src/lab3.cpp
+++ src/lab3.cpp
@@ -7,8 +7,43 @@
 /* The main function, or the "entry function" of the lab3 program, simply calls the other
  * functions to run.
  */
+
+void feet_to_cm()
+{
+    vector<double> feet;
+    double input;
+    vector<double> centimeters;
+
+    while (cin >> input && input != 0)
+    {
+        if (input!=0) {
+            feet.push_back(input);
+        }
+
+        for (int i = 0; i < feet.size(); i++) {
+            centimeters.push_back(feet[i] * 30.48);
+
+        }
+
+        for (int i =0; i < centimeters.size(); i++) {
+            cout << centimeters[i] << endl;
+        }
+
+
+    }
+
+
+}
+
 int main() {
+    cout << "Enter length in feet" << endl;
+    feet_to_cm();
 
     return 0; // Lets the operating system know everything ran successfully.
 }
 
+
+
+
+
+//11 5 0
